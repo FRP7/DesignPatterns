1c5bf22a7809e199a405b3e403e17783ca69dce2
FranciscoPires <frelvaspires@gmail.com>
2020-11-18 15:26:13 +0000

    adapter pattern adicionado

---
+++ AdapterPattern/IMelee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02_12
+{
+    interface IMelee
+    {
+        // Attacks return true if they hit opponent
+        bool AttackFromAbove();
+        bool AttackFromBelow();
+        bool AttackFromTheLeft();
+        bool AttackFromTheRight();
+    }
+}
---
+++ AdapterPattern/Knife.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02_12
+{
+    class Knife : IMelee
+    {
+        // Attacks return true if they hit opponent
+        public bool AttackFromAbove() {
+            Console.WriteLine("Attack from above!");
+            return true;
+        }
+        public bool AttackFromBelow() {
+            Console.WriteLine("Attack from below!");
+            return true;
+        }
+        public bool AttackFromTheLeft() {
+            Console.WriteLine("Attack from the left!");
+            return true;
+        }
+        public bool AttackFromTheRight() {
+            Console.WriteLine("Attack from the right!");
+            return true;
+        }
+    }
+}
